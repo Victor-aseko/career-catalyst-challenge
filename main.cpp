d07fab347f78ba71a43d06bf982dbf370d9198a7
Victor-aseko <101116936+Victor-aseko@users.noreply.github.com>
2022-03-07 23:26:17 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+int addition(int a =4,int b =5)//function prototype
+{
+   return a + b;
+   }
+int main()
+{
+    return 0;
+}
